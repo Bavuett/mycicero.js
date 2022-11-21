a955d615470e4dab3540365e764e1427589f4dcc
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2022-11-21 18:26:18 +0100

    🖋 Adding "Solution" type for proper formatting.

---
+++ src/Types/Solutions.ts
@@ -0,0 +1,59 @@
+interface Minutes {
+    total: number;
+    onFoot: number;
+    onVehicle: number;
+}
+
+interface Meters {
+    total: number;
+    onFoot: number;
+    onVehicle: number | null;
+}
+
+interface Routes {
+    line: Line;
+    departure: Stop;
+    arrival: Stop;
+}
+
+interface Line {
+    description: string;
+    lineNumber: number;
+    company: Company;
+    bookingNeeded: boolean;
+    type: string;
+    extraurban: boolean;
+}
+
+interface Stop {
+    description: string;
+    type: string;
+    location: Location;
+}
+
+interface Location {
+    lat: number;
+    lon: number;
+}
+
+interface Company {
+    code: string;
+    name: string;
+    logoUrl: string;
+}
+
+interface Solution {
+    // TipoSoluzione.
+    type: string;
+    price: number;
+    minutes: Minutes;
+    meters: Meters;
+    routes: Routes[];
+    departure: Date;
+    arrival: Date;
+    co2Emission: number;
+}
+
+export interface Solutions {
+    solutions: Solution[];
+}
