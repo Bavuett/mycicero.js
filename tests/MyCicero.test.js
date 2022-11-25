fc21cb538792d4c40fb6c4ef872a96aa00501600
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2022-11-25 18:38:04 +0100

    🔧 Update Jest tests in order to use new parameters.

--- tests/MyCicero.test.js
+++ tests/MyCicero.test.js
@@ -1,8 +1,6 @@
 const { MyCicero } = require('../dist/index');
 let myCicero;
 
-const date = new Date();
-
 beforeEach(() => {
     myCicero = new MyCicero();
 });
@@ -16,23 +14,28 @@ describe('MyCicero class creation', () => {
 describe('MyCicero class methods', () => {
     test('getSolutions() should resolve without errors', () => {
         myCicero.getSolutions({
-            departure: {
-                lat: 42.4477,
-                lon: 14.2080,
-            },
-            arrival: {
-                lat: 42.4738,
-                lon: 14.1925,
-            },
-        }, {
-            departure: date,
+            locations: {
+                departure: {
+                    lat: 42.4477,
+                    lon: 14.2080,
+                },
+                arrival: {
+                    lat: 42.4738,
+                    lon: 14.1925,
+                },
+            }, 
+            dates: {
+                departure: new Date(),
+            }
         }).resolves;
     });
 
     test('getNearestStops() should resolve without errors', () => {
         myCicero.getNearestStops({
-            lat: 42.4477,
-            lon: 14.2080,
+            location: {
+                lat: 42.4477,
+                lon: 14.2080,
+            }
         }).resolves;
     });
 });
